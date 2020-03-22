19d18bda23040128f62717471fed5ee74ee1c9e4
Milos Ostojic <ostoja_95_cacak@hotmail.com>
2020-03-22 17:58:00 +0100

    Added about content

--- wordpress/wp-content/themes/portfolio/template-about.php
+++ wordpress/wp-content/themes/portfolio/template-about.php
@@ -4,6 +4,38 @@
   */
   get_header();
 ?>
-<h2>Hello about</h2>
+<section class="about">
+  <div class="row">
+    <h1>About</h1>
+    <div class="about-wrapper">
+      <div class="left-part-about">
+  	    <img src="<?php bloginfo('template_directory')?>/assets/man1.jpeg" alt="">
+      </div>
+      <div class="right-part-about">
+        <h2>Hello There</h2>
+        <p>
+        My name is Miloš Otojić. I am Junior Front-End Developer with an eye for design, UX and UI development and a strong desire to learn more and create amazing stuff. I guarantee that I’m driven by a huge motivation to learn new things and start working something different from my earlier experiences.Responsible, ambitious, well organized person,enthusiastic graduate who got very interested in programming, looking for a job or internship in this field. I find myself open minded, passionate and persistent in everything i do. My interests are pretty much diverse, from studying engineering to falling in love with programming. Possesing great communication skills and i love cooperating in teams. Always opet to new project, ideas and people and you can also, expect full commitment in my work anytime.
+        </p>
+        <div class="info">
+  	      <div class="part">
+            <h4>Name:</h4>
+            <p>Milos Ostojic</p>
+          </div>
+          <div class="part">
+            <h4>Phone:</h4>
+            <p>+381 63/624-005</p>
+          </div>
+          <div class="part">
+            <h4>Email:</h4>
+            <p>milosostoja95@gmail.com</p>
+          </div>
+        </div>
+        <div class="about-btn">
+          <a href="" class="contact-btn">Contact me</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 
 <?php get_footer();?>
