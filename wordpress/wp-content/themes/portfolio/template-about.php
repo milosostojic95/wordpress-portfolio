1271871e34b7c684ecd5579caf14852d26857a01
Milos Ostojic <ostoja_95_cacak@hotmail.com>
2020-03-22 18:41:15 +0100

    Modified about page

--- wordpress/wp-content/themes/portfolio/template-about.php
+++ wordpress/wp-content/themes/portfolio/template-about.php
@@ -6,7 +6,7 @@
 ?>
 <section class="about">
   <div class="row">
-    <h1>About</h1>
+    <h1>About Me</h1>
     <div class="about-wrapper">
       <div class="left-part-about">
   	    <img src="<?php bloginfo('template_directory')?>/assets/man1.jpeg" alt="">
@@ -14,24 +14,24 @@
       <div class="right-part-about">
         <h2>Hello There</h2>
         <p>
-        My name is Miloš Otojić. I am Junior Front-End Developer with an eye for design, UX and UI development and a strong desire to learn more and create amazing stuff. I guarantee that I’m driven by a huge motivation to learn new things and start working something different from my earlier experiences.Responsible, ambitious, well organized person,enthusiastic graduate who got very interested in programming, looking for a job or internship in this field. I find myself open minded, passionate and persistent in everything i do. My interests are pretty much diverse, from studying engineering to falling in love with programming. Possesing great communication skills and i love cooperating in teams. Always opet to new project, ideas and people and you can also, expect full commitment in my work anytime.
+        My name is Miloš Otojić. I am Web Developer with an eye for design, UX and UI development and a strong desire to learn more and create amazing stuff. I guarantee that I’m driven by a huge motivation to learn new things and start working something different from my earlier experiences. Responsible, ambitious, well organized person, enthusiastic graduate who got very interested in programming. I find myself open minded, passionate and persistent in everything i do. My interests are pretty much diverse, from studying engineering to falling in love with programming. Possesing great communication skills and i love cooperating in teams. Always opet to new project, ideas and people and you can also, expect full commitment in my work anytime.
         </p>
         <div class="info">
   	      <div class="part">
-            <h4>Name:</h4>
+            <h3>Name:</h3>
             <p>Milos Ostojic</p>
           </div>
           <div class="part">
-            <h4>Phone:</h4>
+            <h3>Phone:</h3>
             <p>+381 63/624-005</p>
           </div>
           <div class="part">
-            <h4>Email:</h4>
-            <p>milosostoja95@gmail.com</p>
+            <h3>Email:</h3>
+            <p>milosostojic95@gmail.com</p>
           </div>
         </div>
         <div class="about-btn">
-          <a href="" class="contact-btn">Contact me</a>
+          <a href="" class="contact-btn">Contact Me</a>
         </div>
       </div>
     </div>
