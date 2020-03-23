a262956cf993acec9eb2547c27a537c844007aa6
Milos Ostojic <ostoja_95_cacak@hotmail.com>
2020-03-23 11:08:08 +0100

    Modified skills page

--- wordpress/wp-content/themes/portfolio/template-skills.php
+++ wordpress/wp-content/themes/portfolio/template-skills.php
@@ -4,6 +4,121 @@
   */
   get_header();
 ?>
-<h2>Hello skills</h2>
-
+  <section class="skills">
+    <div class="row">
+      <h1>Skills</h1>
+      <div class="skills-wrapper">
+        <div class="left-part-skills">
+          <h2>Every Day is a New Challenge</h2>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis non atque error quae velit quibusdam. Optio dolorum veniam praesentium aspernatur rerum, quidem harum, error sed sequi accusamus architecto, odio reiciendis.
+          </p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt, unde nam pariatur reprehenderit dolores quo iure! Enim tenetur beatae inventore quasi eveniet? Assumenda voluptatem voluptatum maxime dicta labore alias quasi?
+          </p>
+          <a href="">Contact Me</a>
+        </div>
+        <div class="right-part-skills">
+          <div class="progress">
+            <h4>Html5</h4>
+            <div class="progress-value">
+              100%
+            </div>
+            <div class="progress-bar"></div>
+          </div>
+          <div class="progress">
+            <h4>Css</h4>
+            <div class="progress-value">
+              100%
+            </div>
+            <div class="progress-bar"></div>
+          </div>
+          <div class="progress" style="width:70%">
+            <h4>JavaScript</h4>
+            <div class="progress-value">
+              70%
+            </div>
+            <div class="progress-bar"></div>
+          </div>
+          <div class="progress" style="width:80%">
+            <h4>WordPress</h4>
+            <div class="progress-value">
+              80%
+            </div>
+            <div class="progress-bar"></div>
+          </div>
+          <div class="progress" style="width:80%">
+            <h4>PhotoShop</h4>
+            <div class="progress-value">
+              80%
+            </div>
+            <div class="progress-bar"></div>
+          </div>
+          <div class="progress">
+            <h4>Webpack</h4>
+            <div class="progress-value">
+              100%
+            </div>
+            <div class="progress-bar"></div>
+          </div>
+          <div class="progress">
+            <h4>Vagrant</h4>
+            <div class="progress-value">
+              100%
+            </div>
+            <div class="progress-bar"></div>
+          </div>
+          <div class="progress">
+            <h4>Git</h4>
+            <div class="progress-value">
+              100%
+            </div>
+            <div class="progress-bar"></div>
+          </div>
+          <div class="progress" style="width:80%">
+            <h4>Magento</h4>
+            <div class="progress-value">
+              80%
+            </div>
+            <div class="progress-bar"></div>
+          </div>
+          <div class="progress" style="width:80%">
+            <h4>CraftCMS</h4>
+            <div class="progress-value">
+              80%
+            </div>
+            <div class="progress-bar"></div>
+          </div>
+          <div class="progress" style="width:30%">
+            <h4>Php</h4>
+            <div class="progress-value">
+              30%
+            </div>
+            <div class="progress-bar"></div>
+          </div>
+          <div class="progress">
+            <h4>jQuery</h4>
+            <div class="progress-value">
+              100%
+            </div>
+            <div class="progress-bar"></div>
+          </div>
+          <div class="progress">
+            <h4>Sass</h4>
+            <div class="progress-value">
+              100%
+            </div>
+            <div class="progress-bar"></div>
+          </div>
+          <div class="progress">
+            <h4>Bootstrap</h4>
+            <div class="progress-value">
+              100%
+            </div>
+            <div class="progress-bar"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 <?php get_footer();?>
