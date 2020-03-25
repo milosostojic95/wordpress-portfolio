5d4ec65604328efb553032029c905ba63f4136bd
Milos Ostojic <ostoja_95_cacak@hotmail.com>
2020-03-25 19:50:31 +0100

    Added contactform.php

--- wordpress/wp-content/themes/portfolio/index.php
+++ wordpress/wp-content/themes/portfolio/index.php
@@ -1,2 +1,3 @@
 <?php get_header();?>
+  hello from index
 <?php get_footer();?>
--- wordpress/wp-content/themes/portfolio/template-contact.php
+++ wordpress/wp-content/themes/portfolio/template-contact.php
@@ -36,7 +36,7 @@
         </div>
         <div class="contact-form">
           <h2>Send Me a Message</h2>
-          <form method="post" id="contact-form">
+          <form method="post" id="contact-form" action="contactform.php">
             <div class="form-group form">
               <label for="">Name*</label>
               <input type="text" name="name" class="form-control" placeholder="Name" required>
@@ -47,13 +47,13 @@
             </div>
             <div class="subject form">
               <label for="">Subject*</label>
-              <input type="email" name="subject" class="form-control" placeholder="Subject" required>
+              <input type="subject" name="subject" class="form-control" placeholder="Subject" required>
             </div>
             <div class="form-message form">
               <label for="">Message*</label>
               <textarea name="message" id="" cols="30" rows="5" class="form-control" placeholder="Text Message" required></textarea>
             </div>
-            <button type="submit" class="send-btn" id="form-contact-submit">Send Message</button>
+            <button type="submit" name="submit" class="send-btn" id="form-contact-submit">Send Message</button>
           </form>
         </div>
       </div>
